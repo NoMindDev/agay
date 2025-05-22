638561c1e46597655e7cba187f8f7352d302003e
kuenzang24 <kuenzang24.dev@gmail.com>
2025-05-22 21:32:07 +0600

    made the header and side bar sick with when the child is scrollable

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -9,11 +9,21 @@ import Header from "@/components/website/Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen bg-white">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Header />
-        {children}
+    <div className="flex h-screen overflow-hidden bg-white">
+      {/* Fixed Sidebar */}
+      <div className="w-[240px] shrink-0">
+        <Sidebar />
+      </div>
+
+      {/* Main Content Area */}
+      <div className="flex-1 flex flex-col overflow-hidden">
+        {/* Fixed Header */}
+        <div className="shrink-0">
+          <Header />
+        </div>
+
+        {/* Scrollable Content Area */}
+        <div className="flex-1 overflow-auto bg-white p-4">{children}</div>
       </div>
     </div>
   );
